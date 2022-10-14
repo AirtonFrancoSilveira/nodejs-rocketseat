315f94ab51aaed00487eed2090e4f5d5086948c9
AirtonFrancoSilveira <airton.franco@rvmdigital.com.br>
2022-10-14 20:14:24 -0300

    Tempo da aula 41:48

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const app = express()
 
-app.get("/primeira-rota", (req, res) => {
-    return res.json({
-        message: "Acessou a primeira Rota com Nodemon"
-    })
+// app.get("/primeira-rota", (req, res) => {
+//     return res.json({
+//         message: "Acessou a primeira Rota com Nodemon"
+//     })
+// })
+
+const products = []
+
+//** Post, Get, Put, Delete */
+
+app.post("/products", (req, res) => {
+    //Nome e Preço
+    return res
 })
 
 app.listen(4003, () => console.log("Está rodando na porta 4003"))
